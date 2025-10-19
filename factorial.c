a57ea7056ef2fd6562ea36736d24963ddcd52ff8
Tapaswi555 <pulleputapaswi@gmail.com>
2025-10-19 14:26:06 +0530

    updated

--- factorial.c
+++ factorial.c
@@ -2,15 +2,15 @@
 
 int main() {
     int n;
-    unsigned long long factorial = 1;
+    long long int factorial = 1;
 
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    for(int i = 1; i <= n; ++i) {
+    for(int i = 1; i <= n; i++) {
         factorial *= i;
     }
 
-    printf("Factorial of %d = %llu\n", n, factorial);
+    printf("Factorial of %d = %lld\n", n, factorial);
     return 0;
 }
